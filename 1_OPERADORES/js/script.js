eb0b9a13b962a123aeca8ef5fab890d25f5fdc75
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-03-31 22:05:15 -0300

    Operadores_JS

---
+++ 1_OPERADORES/js/script.js
@@ -0,0 +1,43 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.6);
+console.log(typeof -7);
+
+// 2 - Operadores aritméticos
+console.log(2 + 5);
+console.log(2 * 5);
+console.log(2 - 5);
+console.log(2 / 5);
+
+console.log(5 + (4 * 2));
+
+// 3  - Special Numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(5 * "patrick");
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Patrick");
+console.log('Patrick');
+console.log(`Patrick`);
+
+// 5 - Caracteres especiais
+console.log("Testando a \n quebra de linha");
+
+console.log("Espaçamento \t de Tab");
+
+// 6 - Concatenação
+console.log("Meu" + " Malvado" + " Favorito");
+
+// 7 - Interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+//console.log(`Podemos executar qualquer coisa aqui ${console.log("Teste")}`);
+
+// 8 - Booleanos
+console.log(true);
+console.log(typeof false);
+console.log(5 > 20);
+console.log(30 > 10);
