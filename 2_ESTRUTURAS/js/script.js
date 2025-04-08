6d2e9ccf37b21c5f6f0b308ff035a55d24b514d8
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-04-07 21:53:14 -0300

    Estruturas_programação

---
+++ 2_ESTRUTURAS/js/script.js
@@ -0,0 +1,41 @@
+// 1 - variáveis
+let nome = "Patrick";
+console.log(nome);
+
+nome = "Patrick Corrêa";
+console.log(nome);
+
+const idade = 31;
+console.log(idade);
+//idade = 32 (Const tem um valor fixo)
+
+// 2 - Mais sobre variáveis
+
+//let 2teste = "inválido";
+//let @teste = "inválido";
+
+let a = 10, b = 20, c = 30
+console.log(a, b, c);
+
+const nomecompleto = "Patrick Corrêa"
+
+const nomeCompleto = "Patrick Corrêa da Silva"
+
+console.log(nomecompleto)
+console.log(nomeCompleto)
+
+
+let _teste = "ok";
+let $teste = "OK";
+
+console.log(_teste, $teste);
+
+// 3.1 - Funçôes Js: prompt
+const age = prompt("Digite sua idade: ");
+
+console.log(`Você tem ${age} anos.`)
+
+// 3.2 - Funçôes Js: alert
+alert("Testando");
+const z = 10;
+alert(`O número é ${z}`);
