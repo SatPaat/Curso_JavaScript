bc29257f8bf3fe4e37e82d8f8a72f50c2e3027fe
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-04-14 22:40:25 -0300

    Funções

---
+++ 3_FUNÇÕES/Js/script.js
@@ -0,0 +1,29 @@
+// 1 - Criando um função
+function minhaFuncao(){
+    console.log("Testando")
+} 
+
+minhaFuncao();
+minhaFuncao();
+
+const minhaFuncaoEmVariavel = function () {
+    console.log("Minha função em variavel")
+}
+
+minhaFuncaoEmVaria();
+
+function funcaoComParametro(txt){
+    console.log(`Imprimindo: ${txt}`)
+}
+
+funcaoComParametro("Imprimindo alguma coisa");
+
+funcaoComParametro("Outra função!");
+
+function teste(){
+    console.log("Finalizando a criação de função!")
+}
+
+teste();
+
+// 2 - 
