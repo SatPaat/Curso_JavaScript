29fcedfc51256ced91fa23adc6751a2e29e894da
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-04-24 22:00:49 -0300

    Update script.js

--- 4_ARRAYS_E_OBJ/js/script.js
+++ 4_ARRAYS_E_OBJ/js/script.js
@@ -93,3 +93,131 @@ console.log(Object.keys(obj2));
 console.log(Object.keys(car));
 
 console.log(Object.entries(car));
+
+// 9 - Mutação
+const a = {
+    name: "Patrick"
+}
+
+const b = a;
+
+console.log(a);
+console.log(b);
+
+console.log(a === b);
+
+a.age = 23;
+
+console.log(a);
+console.log(b);
+
+delete b.age;
+
+console.log(a);
+console.log(b);
+
+// 10 - Loop em array
+const users = ["Patrick", "Matheus", "João", "Pedro"]
+
+for(let i = 0; i < users.length; i++){
+    console.log(`Listando o usuário: ${users(i)}`);
+}
+
+// 11 - Métodos de arrays: push e pop (Adiciona itens no final e remove elementos do final do array)
+const array = ["a", "b", "c"]
+
+array.push("d")
+
+console.log(array);
+
+array.pop()
+
+console.log(array);
+
+const itemRemovido = array.pop()
+
+console.log(itemRemovido);
+
+console.log(array);
+
+array.push("z", "x", "y")
+
+console.log(array);
+
+// 12 - Métodos de arrays: shift e unshift (Remove o primeiro elemento e adiciona itens no inicio do array)
+const letters = ["a", "b", "c"]
+
+const letter = letters.shift()
+
+console.log(letter);
+console.log(letters);
+
+letters.unshift("p", "q", "r")
+letters.unshift("s")
+
+console.log(letters)
+
+// 13 - IndexOf e lastIndexOf
+const myElements = ["Morango", "Manga", "Abacaxi", "Banana", "Manga"]
+
+console.log(myElements.indexOf("Morango"));
+console.log(myElements.indexOf("Manga"));
+
+console.log(myElements[2]);
+console.log(myElements[myElements.indexOf("Manga")]);
+
+console.log(myElements.lastIndexOf("Manga"));
+
+console.log(myElements.lastIndexOf("Amora")); //Varios métodos que não encontram o resultado retornam -1
+
+// 14 - slice
+const testeSlice = ["a", "b", "c", "d", "e", "f"]
+
+const subArray = testeSlice.slice(2 , 4)
+
+console.log(subArray);
+console.log(testeSlice);
+
+const subArray2 = testeSlice.slice(2 , 4 + 1)
+
+console.log(subArray2);
+
+const subArray3 = testeSlice.slice(10, 20)
+console.log(subArray3);
+
+const subArray4 = testeSlice.slice(2);
+console.log(subArray4);
+
+// 15 - foreach
+const num = [1, 2, 3, 4, 5]
+
+num.forEach((numero) => {
+    console.log(`O número é ${numero}`)
+})
+
+const posts = [
+    {title: "Primeiro post", category: "PHP"},
+    {title: "Segundo post", category: "JAVA"},
+    {title: "Terceiro post", category: "PYTHON"},
+];
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria ${post.category}`)
+})
+
+// 16 - Includes
+const brand = ["BMW", "FIAT", "VW"]
+
+console.log(brand.includes("FIAT")); //true
+console.log(brand.includes("KIA")); //false
+
+if(brand.includes("BMW")){
+    console.log("Há carros desta marca!");
+}
+
+// 17 - Reverse
+const reverseTest = [1, 2, 3, 4, 5]
+
+reverseTest.reverse()
+
+console.log(reverseTest);
