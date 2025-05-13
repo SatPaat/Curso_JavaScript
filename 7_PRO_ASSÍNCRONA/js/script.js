a3376f705b218fcfe9ec6a655e0c52eff8c4e9bb
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-05-12 21:26:28 -0300

    Pro_assíncrona

---
+++ 7_PRO_ASSÍNCRONA/js/script.js
@@ -0,0 +1,41 @@
+// 1 - setTimeout
+console.log("Ainda não executou");
+
+setTimeout(function(){
+    console.log("Requisição assíncrona");
+}, 2000);
+
+console.log("Ainda não executou 2");
+
+// 2 - setInterval
+console.log("Ainda não começou");
+
+//setInterval(function(){
+    //console.log("Intervalo assíncrono")
+//}, 3000);
+
+console.log("Ainda não começou 2");
+
+// 3 - promisses
+const promessa = Promise.resolve(5 + 5)
+
+console.log("Algum código")
+
+promessa.then(value => {
+    console.log(`A soma é ${value}`);
+    return value;
+})
+.then((value) => value - 1)
+.then((value) => console.log(`Agora é ${value}`));
+
+console.log("Outro código")
+
+// 4 - Erro na promise
+Promise.resolve(4 * "asd")
+
+.then((n) =>{
+    if(Number.isNaN(n)){
+        throw new Error("Valores inválidos")
+    }
+})
+.catch((err) => console.log(`Um erro ocorreu ${err}`));
