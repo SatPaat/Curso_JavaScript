9f39cfbbcf6e0f936a513a6459dde3cde252481e
Patrick Corrêa <patrickcorrea250@gmail.com>
2025-07-03 22:04:35 -0300

    Update script.js

--- 11_JS_MODERNO/js/script.js
+++ 11_JS_MODERNO/js/script.js
@@ -117,3 +117,84 @@ const userName = "Patrick"
 const age = 23
 
 console.log(`O nome do usuário é ${userName} e sua idade é ${age} anos`); 
+
+
+// 6 - destructuring
+const fruits = ["Abacaxi", "melão", "laranja", "manga"]
+
+const [f1, f2, f3, f4] = fruits
+
+console.log(f1, f4);
+
+
+
+const productDetails ={
+    name: "Mouse",
+    price: 15,
+    category: "periferico",
+    color: "preto"
+}
+
+const {name: productName, price, category, color} = productDetails;
+
+console.log(`O nome do produto é ${productName}, ele custa R$ ${price} e é da cor ${color}`);
+
+
+// 7 - spread operator
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+
+const a3 = [...a1, ...a2];
+console.log(a3);
+
+const a4 = [0, ...a3, 7];
+console.log(a4);
+
+
+const carName = {name: "gol"};
+const carBrand = {brand: "vw"};
+const otherInfos = {km: 100000, price: 49999};
+
+const car = {...name, ...carBrand, ...otherInfos};
+console.log(car);
+
+// 8 - classes
+class Product{
+    constructor(name, price){
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount ) / 100)
+    }
+}
+
+const shirt = new Product("camisa gola V", 20)
+
+console.log(shirt);
+
+console.log(shirt.productWithDiscount(10));
+console.log(shirt.productWithDiscount(15));
+
+// 9 - Herança
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors){
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log("As cores são: ")
+        this.colors.forEach((color) => {
+            console.log(color);
+        })
+    }
+}
+
+const hat = new ProductWithAttributes ("Chapéu", 25, ["vermelho", "amarelo", "verde"])
+console.log(hat)
+
+console.log(hat.name);
+
+hat.showColors();
